79f814bdbfb259ad2928396e8dd8e218638cd501
Oscar Javier Idrobo Lopez <oidrobo@bancodebogota.com.co>
2022-01-25 16:49:18 -0500

    test check ok

--- src/test/groovy/co/com/bancodebogota/client/RegresClientSpec.groovy
+++ src/test/groovy/co/com/bancodebogota/client/RegresClientSpec.groovy
@@ -1,5 +1,6 @@
 package co.com.bancodebogota.client
 
+import co.com.bancodebogota.context.account.limits.domain.user.UsersDto
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import jakarta.inject.Inject
 import spock.lang.Specification
@@ -8,11 +9,25 @@ import spock.lang.Specification
 class RegresClientSpec extends Specification {
 
     @Inject
-    RegresClient accountLimitsClient
+    RegresClient regresClient
 
+    void 'get information User'(){
+        when: "get information client"
+        def informationUser = regresClient.informationUser(idUser)
 
-    void 'test health controller'(){
-        expect:
-        accountLimitsClient.health() == "Healthy OK!"
+        then: "the tags pags is include"
+        informationUser.page == expect
+
+        where: "if the idUser is equal the value "
+        idUser | expect
+        1      | 0
+    }
+
+    void 'Validate structure information User'(){
+        when: "get information client"
+        def informationUser = regresClient.informationUser(1)
+
+        then: "The information is the UserDto Class"
+        informationUser.getClass() == UsersDto.class
     }
 }

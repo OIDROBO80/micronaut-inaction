80d2b43f3c6c924af29c8b887858779ae99766d3
Oscar Javier Idrobo Lopez <oidrobo@bancodebogota.com.co>
2022-01-26 11:30:38 -0500

    merge SP1

--- src/test/groovy/co/com/bancodebogota/client/ReqresClientSpec.groovy
+++ src/test/groovy/co/com/bancodebogota/client/ReqresClientSpec.groovy
@@ -1,18 +1,35 @@
 package co.com.bancodebogota.client
 
+import co.com.bancodebogota.context.account.limits.domain.user.DataUser
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import jakarta.inject.Inject
 import spock.lang.Specification
+
 
 @MicronautTest
 class ReqresClientSpec extends Specification {
 
     @Inject
-    ReqresClient accountLimitsClient
+    ReqresClient reqresClient
 
+    void 'get information User'(){
+        when: "get information client"
+        def informationUser = reqresClient.informationUserData(idUser)
 
-    void 'test health controller'(){
-        expect:
-        accountLimitsClient.health() == "Healthy OK!"
+        then: "the tags id is include"
+        informationUser.data.id == expect
+
+        where: "if the idUser is equal the value "
+        idUser | expect
+        2      | 2
+        3      | 3
+    }
+
+    void 'Validate structure information User'(){
+        when: "get information client"
+        def informationUser = reqresClient.informationUserData(1)
+
+        then: "The information is the UserDto Class"
+        informationUser.getClass() == DataUser.class
     }
 }

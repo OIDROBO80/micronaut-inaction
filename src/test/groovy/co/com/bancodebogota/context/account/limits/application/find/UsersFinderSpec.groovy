6d378bd503978e7e3bb1fd961a0b40e1dd413c01
Oscar Javier Idrobo Lopez <oidrobo@bancodebogota.com.co>
2022-01-27 09:28:38 -0500

    unit test user finder

--- src/test/groovy/co/com/bancodebogota/context/account/limits/application/find/UsersFinderSpec.groovy
+++ src/test/groovy/co/com/bancodebogota/context/account/limits/application/find/UsersFinderSpec.groovy
@@ -2,28 +2,33 @@ package co.com.bancodebogota.context.account.limits.application.find
 
 
 import co.com.bancodebogota.context.account.limits.domain.user.Data
+import co.com.bancodebogota.context.account.limits.domain.user.DataUser
+import co.com.bancodebogota.context.account.limits.infrastructure.ApiReqres
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import spock.lang.Specification
 import spock.lang.Subject
 
 @MicronautTest
 class UsersFinderSpec extends Specification {
-    static final Data ACCOUNT_LIMITS_QUERY = new Data()
+    static final DataUser dataUser = new DataUser()
 
-    APIRegresRepository accountLimitsRepository = Mock()
+    ApiReqres apiReqres = Mock()
 
     @Subject
-    UsersFinder accountLimitsFinder = new UsersFinder(accountLimitsRepository)
+    UsersFinder usersFinder = new UsersFinder(apiReqres)
 
     def "should find account limits successfully"() {
-        given: "An account number"
-        accountLimitsRepository.getAccountLimits(_ as String) >> ACCOUNT_LIMITS_QUERY
+        given: "An information user"
+        apiReqres.informationUserData([*_]) >> dataUser
 
-        when: "An account number is passed to find account limits"
-        def result = accountLimitsFinder.run(_ as String)
+        when: "An id number is passed to find information user"
+        def result = usersFinder.informationUserData([*_])
 
-        then: "An account limits should be returned"
-        result == ACCOUNT_LIMITS_QUERY
+        then: "An information user should be returned"
+        result == dataUser
+
+        /*where:
+        idUser = 2*/
     }
 
 }

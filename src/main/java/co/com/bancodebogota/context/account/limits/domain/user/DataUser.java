838c13ee3deb3968a2d8fff429e9b53052e8ec2f
Oscar Javier Idrobo Lopez <oidrobo@bancodebogota.com.co>
2022-01-28 12:03:14 -0500

    include class Response

--- src/main/java/co/com/bancodebogota/context/account/limits/domain/user/DataUser.java
+++ src/main/java/co/com/bancodebogota/context/account/limits/domain/user/DataUser.java
@@ -1,14 +1,15 @@
 package co.com.bancodebogota.context.account.limits.domain.user;
 
+import co.com.bancodebogota.context.account.limits.domain.response.Response;
 import io.micronaut.core.annotation.Introspected;
 
 @Introspected
 public class DataUser{
     public Data data;
     public Support support;
-    public String message;
+    public Response response = new Response();
 
-    public DataUser(String message) {
-        this.message = message;
+    public DataUser(String message) {//
+        this.response.message = message;
     }
 }

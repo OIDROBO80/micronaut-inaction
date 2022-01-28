69ccb187272056d8c885316003cba95e2faa68ad
Oscar Javier Idrobo Lopez <oidrobo@bancodebogota.com.co>
2022-01-28 12:01:49 -0500

    include method to response controller get user

---
+++ src/main/java/co/com/bancodebogota/context/account/limits/domain/Utils.java
@@ -0,0 +1,12 @@
+package co.com.bancodebogota.context.account.limits.domain;
+
+import co.com.bancodebogota.context.account.limits.domain.user.DataUser;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+
+public class Utils {
+
+    public  static HttpResponse validateResponse(DataUser dataUser) {
+        return  HttpResponse.status(HttpStatus.valueOf(dataUser.response.statusCode)).body(dataUser);
+    }
+}

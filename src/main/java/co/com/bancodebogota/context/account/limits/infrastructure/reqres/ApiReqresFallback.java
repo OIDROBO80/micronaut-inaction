05d1995c955cee8b124028f442bc89651497b239
Brayan Felipe Pardo Cortes <92054081+bfpard@users.noreply.github.com>
2022-01-31 09:30:26 -0500

    V2.3-REFACTOR - Fallback Undefined

--- src/main/java/co/com/bancodebogota/context/account/limits/infrastructure/reqres/ApiReqresFallback.java
+++ src/main/java/co/com/bancodebogota/context/account/limits/infrastructure/reqres/ApiReqresFallback.java
@@ -1,5 +1,20 @@
 package co.com.bancodebogota.context.account.limits.infrastructure.reqres;
 
-public class ApiReqresFallback{
 
+import co.com.bancodebogota.context.account.limits.domain.user.DataUser;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.retry.annotation.Fallback;
+
+@Fallback
+public class ApiReqresFallback implements ApiReqres{
+
+    @Override
+    public HttpResponse<DataUser> getUserById(int idUser) {
+        return null;
+    }
+
+    @Override
+    public HttpResponse<Object> createUser() {
+        return null;
+    }
 }

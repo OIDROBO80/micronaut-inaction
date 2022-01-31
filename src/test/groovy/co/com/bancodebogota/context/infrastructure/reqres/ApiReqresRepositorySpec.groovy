a4db79aafbca9b34f3302076a220539912b6ea1f
Oscar Javier Idrobo Lopez <oidrobo@bancodebogota.com.co>
2022-01-31 09:19:57 -0500

    create file unit test

---
+++ src/test/groovy/co/com/bancodebogota/context/infrastructure/reqres/ApiReqresRepositorySpec.groovy
@@ -0,0 +1,40 @@
+package co.com.bancodebogota.context.infrastructure.reqres
+
+import co.com.bancodebogota.context.application.Information
+import co.com.bancodebogota.context.domain.interfaces.UserRepository
+import co.com.bancodebogota.context.domain.user.DataUser
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.annotation.MockBean
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import jakarta.inject.Inject
+import spock.lang.Specification
+import spock.lang.Subject
+
+@MicronautTest
+class ApiReqresRepositorySpec extends Specification{
+
+    static final DataUser dataUser = new DataUser("");
+
+    @Inject
+    @Client("/")
+    HttpClient client;
+
+    @Subject
+    ApiReqresRepository apiReqresRepository = new ApiReqresRepository(client)
+
+    def "should return information user by Id"() {
+        given: "An information user"
+        client.toBlocking().retrieve(_ as String,DataUser.class) >> dataUser
+
+        when: "An get information by Id"
+        def result = apiReqresRepository.getUserData(idUser)
+
+        then: "An information user is resolve"
+        result == dataUser
+
+        where: "The idUser is any number"
+        idUser << [0]
+    }
+}
+

cc08e40b41122300217b6c71dd1b0405a6deebb7
Brayan Felipe Pardo Cortes <92054081+bfpard@users.noreply.github.com>
2022-01-31 09:33:32 -0500

    V2.3-REFACTOR - Imformation to UserRepository

--- src/main/java/co/com/bancodebogota/api/ReqresController.java
+++ src/main/java/co/com/bancodebogota/api/ReqresController.java
@@ -1,7 +1,6 @@
 package co.com.bancodebogota.api;
 
-
-import co.com.bancodebogota.context.account.limits.application.data.Information;
+import co.com.bancodebogota.context.account.limits.domain.interfaces.UserRepository;
 import co.com.bancodebogota.context.account.limits.domain.user.DataUser;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
@@ -11,22 +10,20 @@ import io.micronaut.http.annotation.Post;
 @Controller("/api/${api.version:v1}")
 public final class ReqresController {
 
-    private final Information information;
+    private final UserRepository userRepository;
 
-    public ReqresController(Information information) {
-
-        this.information = information;
-
+    public ReqresController(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Get(value="users/{iduser}")
     public DataUser GetUser(@PathVariable("iduser") int idUser) {
-        return information.byId(idUser);
+        return userRepository.getUserData(idUser);
     }
 
     @Post(value = "users/create")
     public Object CreateUser(){
-        return information.create();
+        return userRepository.createUser();
     }
 
 }

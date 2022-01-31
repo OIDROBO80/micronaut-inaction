c14ac184524b299e2f403da3b754dd806bcad725
Brayan Felipe Pardo Cortes <92054081+bfpard@users.noreply.github.com>
2022-01-31 09:35:04 -0500

    V2.3-REFACTOR - The comment was deleted

--- src/main/java/co/com/bancodebogota/context/account/limits/domain/interfaces/UserRepository.java
+++ src/main/java/co/com/bancodebogota/context/account/limits/domain/interfaces/UserRepository.java
@@ -1,11 +1,8 @@
 package co.com.bancodebogota.context.account.limits.domain.interfaces;
 
 import co.com.bancodebogota.context.account.limits.domain.user.DataUser;
-import co.com.bancodebogota.context.account.limits.domain.user.DataUsers;
-import io.micronaut.http.HttpResponse;
 
 public interface UserRepository {
-    // Get User Reqres by id
     DataUser getUserData(int idUser);
     Object createUser();
 }

0dab41dbb26555a4701105dd07c0bb01e726a5e8
Brayan Felipe Pardo Cortes <92054081+bfpard@users.noreply.github.com>
2022-01-31 10:01:13 -0500

    V2.3-REFACTOR - Try - Catch Implementation

--- src/main/java/co/com/bancodebogota/context/account/limits/infrastructure/reqres/ApiReqresRepository.java
+++ src/main/java/co/com/bancodebogota/context/account/limits/infrastructure/reqres/ApiReqresRepository.java
@@ -1,9 +1,11 @@
 package co.com.bancodebogota.context.account.limits.infrastructure.reqres;
 
 import co.com.bancodebogota.context.account.limits.domain.interfaces.UserRepository;
+import co.com.bancodebogota.context.account.limits.domain.user.Data;
 import co.com.bancodebogota.context.account.limits.domain.user.DataUser;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
@@ -21,16 +23,31 @@ public class ApiReqresRepository implements UserRepository {
 
     @Override()
     public DataUser getUserData(int idUser) {
-        HttpResponse<DataUser> data = apiReqres.getUserById(idUser);
-        System.out.println(data.code());
-        return data.body();
+        try {
+            HttpResponse<DataUser> data = apiReqres.getUserById(idUser);
+            System.out.println(data.code());
+            return data.body();
+        }catch (HttpClientResponseException exception){
+            System.out.println(exception.getStatus().getCode());
+            System.out.println(exception.getMessage());
+            DataUser dataUser = new DataUser(exception.getMessage());
+            return dataUser;
+        }
     }
 
     @Override
     public Object createUser() {
-        HttpResponse<Object> data = apiReqres.createUser();
-        System.out.println(data.code());
-        return data.body();
+        try {
+            HttpResponse<Object> data = apiReqres.createUser();
+            System.out.println(data.code());
+            return data.body();
+        }catch (HttpClientResponseException exception){
+            System.out.println(exception.getStatus().getCode());
+            System.out.println(exception.getMessage());
+            DataUser dataUser = new DataUser(exception.getMessage());
+            return dataUser;
+        }
+
     }
 
 }

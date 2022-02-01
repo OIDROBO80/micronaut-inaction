11f888c9c92b8601fa84094f0cfe59ed926bf419
Oscar Javier Idrobo Lopez <oidrobo@bancodebogota.com.co>
2022-02-01 12:09:05 -0500

    delete file dont use

--- src/test/groovy/co/com/bancodebogota/context/infrastructure/reqres/httpClient.groovy
+++
@@ -1,9 +0,0 @@
-package co.com.bancodebogota.context.infrastructure.reqres
-
-import io.micronaut.http.client.HttpClient
-import io.micronaut.test.annotation.MockBean
-
-@MockBean(HttpClient)
-HttpClient httpClient() {
-    return Mock(HttpClient)
-}

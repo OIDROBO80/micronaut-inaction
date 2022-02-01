d994d58e0bfc5b51c32fd28191104abf27613d90
Oscar Javier Idrobo Lopez <oidrobo@bancodebogota.com.co>
2022-02-01 09:05:09 -0500

    fix names

--- src/main/java/co/com/bancodebogota/context/domain/interfaces/User.java
+++ src/main/java/co/com/bancodebogota/context/domain/interfaces/User.java
@@ -1,11 +1,11 @@
 package co.com.bancodebogota.context.domain.interfaces;
 
+import co.com.bancodebogota.context.domain.response.Response;
 import co.com.bancodebogota.context.domain.user.DataUser;
 import co.com.bancodebogota.context.domain.user.DataUsers;
 
 public interface User {
-    Object myselfApi();
     DataUser byId(int idUser);
     DataUsers byPage(int idPage);
-    Object create();
+    Response create();
 }

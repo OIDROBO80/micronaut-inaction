0ee9f3d47bae30edc006abf98507d7422d02fac2
Oscar Javier Idrobo Lopez <oidrobo@bancodebogota.com.co>
2022-02-01 09:05:19 -0500

    fix implemtation

--- src/main/java/co/com/bancodebogota/api/ReqresController.java
+++ src/main/java/co/com/bancodebogota/api/ReqresController.java
@@ -3,6 +3,7 @@ package co.com.bancodebogota.api;
 
 import co.com.bancodebogota.context.domain.Utils;
 import co.com.bancodebogota.context.domain.interfaces.User;
+import co.com.bancodebogota.context.domain.response.Response;
 import co.com.bancodebogota.context.domain.user.DataUser;
 import co.com.bancodebogota.context.domain.user.DataUsers;
 import io.micronaut.http.HttpResponse;
@@ -10,34 +11,30 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.http.annotation.Post;
+import jakarta.inject.Inject;
 
 @Controller("/api/${api.version:v1}")
 public final class ReqresController {
-
+    @Inject
     private final User user;
 
     public ReqresController(User user) {
         this.user = user;
     }
 
-    @Get(value = "users/apimyself")
-    public Object getApiMyself(){
-        return user.myselfApi();
-    }
-
     @Get(value="users/{iduser}")
     public HttpResponse<DataUser> GetUser(@PathVariable("iduser") int idUser) {
-        return Utils.validateResponse(user.byId(idUser));
+        return Utils.validateDataUser(user.byId(idUser));
     }
 
     @Get(value = "users/list/{idPage}")
-    public DataUsers GetUsersList(@PathVariable("idPage")int idPage){
-        return user.byPage(idPage);
+    public HttpResponse<DataUsers>  GetUsersList(@PathVariable("idPage")int idPage){
+        return Utils.validateDataUsers(user.byPage(idPage));
     }
 
    @Post(value = "users")
-    public Object createUser(){
-        return user.create();
+    public HttpResponse<Response> createUser(){
+        return Utils.validateResponse(user.create());
     }
 
 

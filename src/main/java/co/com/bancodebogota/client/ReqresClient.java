c405c197c0aaf628ff972368f8aae311b79cd00d
Oscar Javier Idrobo Lopez <oidrobo@bancodebogota.com.co>
2022-02-01 09:41:04 -0500

    fix name interfaces

--- src/main/java/co/com/bancodebogota/client/ReqresClient.java
+++ src/main/java/co/com/bancodebogota/client/ReqresClient.java
@@ -1,28 +1,19 @@
 package co.com.bancodebogota.client;
 
+import co.com.bancodebogota.context.domain.response.Response;
 import co.com.bancodebogota.context.domain.user.DataUser;
 import co.com.bancodebogota.context.domain.user.DataUsers;
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.*;
 
 public interface ReqresClient {
 
-    Object myselfApi();
-
-    DataUser byId(@PathVariable("iduser") int idUser);
+    HttpResponse<DataUser> GetUser(@PathVariable("iduser") int idUser);
 
 
-    DataUsers byPage(@PathVariable("idPage")int idPage);
+    HttpResponse<DataUsers> GetUsersList(@PathVariable("idPage")int idPage);
 
-    Object create();
-
-
-    //HttpResponse createUser();
-
-    /*
-    HttpResponse updateUser(@PathVariable("idUser") int idUser);
-
-
-    HttpResponse deleteUser(@PathVariable("idUser") int idUser);*/
+    HttpResponse<Response> createUser();
 
 
 

e550c4d37593d19cad599201520e54c78f33bfe8
Oscar Javier Idrobo Lopez <oidrobo@bancodebogota.com.co>
2022-02-01 09:04:56 -0500

    include signs to validate

--- src/main/java/co/com/bancodebogota/context/domain/Utils.java
+++ src/main/java/co/com/bancodebogota/context/domain/Utils.java
@@ -1,12 +1,21 @@
 package co.com.bancodebogota.context.domain;
 
+import co.com.bancodebogota.context.domain.response.Response;
 import co.com.bancodebogota.context.domain.user.DataUser;
+import co.com.bancodebogota.context.domain.user.DataUsers;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
 
 public class Utils {
 
-    public  static HttpResponse<DataUser> validateResponse(DataUser dataUser) {
+    public  static HttpResponse<DataUser> validateDataUser(DataUser dataUser) {
         return  HttpResponse.status(HttpStatus.valueOf(dataUser.response.statusCode)).body(dataUser);
+    }
+    public  static HttpResponse<DataUsers> validateDataUsers(DataUsers dataUsers) {
+        return  HttpResponse.status(HttpStatus.valueOf(dataUsers.response.statusCode)).body(dataUsers);
+    }
+
+    public  static HttpResponse<Response> validateResponse(Response response) {
+        return  HttpResponse.status(HttpStatus.valueOf(response.statusCode)).body(response);
     }
 }
